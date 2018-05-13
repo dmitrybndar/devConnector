992c9ebb0ddaf5391a561cb0ed688adc34e2b486
dmitrybndar <dmitry.bndar@gmail.com>
2018-05-13 17:25:27 +0300

    Prepared to deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://dreamlike:dgbple90@ds149122.mlab.com:49122/devconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
